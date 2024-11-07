cff0e7a97cd7185477f3f3f2973ef57f8a3fecfe
Fredrik von Post <fredrik.v.post@gmail.com>
2024-11-07 21:44:42 +0100

    Console UI WIP

---
+++ ConsoleUI.cs
@@ -0,0 +1,76 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace ParkingDeluxe
+{
+    internal class ConsoleUI
+    {
+        private ParkingSpace _garage;
+
+        public ConsoleUI(ParkingSpace garage)
+        {
+            _garage = garage;
+        }
+
+        internal void Start()
+        {
+            bool runAgain = true;
+            while (runAgain)
+            {
+                Console.WriteLine("P or C");
+                ConsoleKeyInfo keyPress = Console.ReadKey(true);
+                switch (keyPress.Key)
+                {
+                    case ConsoleKey.P:
+                        _garage.Park(Utilities.GetRandomVehicle());
+                        break;
+                    case ConsoleKey.C:
+                        Utilities.CheckoutRandomVehicle(_garage);
+                        break;
+                    case ConsoleKey.Q:
+                        runAgain = false;
+                        break;
+                }
+                Console.Clear();
+                for (int i = 0; i < _garage.Count; i++)
+                {
+                    {
+                        Console.WriteLine($"{_garage.ParkingSpots[i].ID}: {_garage.ParkingSpots[i].OccupyingVechicle}");
+                    }
+                }
+                ShowMainMenu();
+                ListParkedVehicles();
+            }
+        }
+
+        private void ShowMainMenu()
+        {
+            Console.WriteLine("1. Parkera ett fordon");
+            Console.WriteLine("1. Checka ut ett fordon");
+            Console.WriteLine("Avsluta");
+
+        }
+
+        internal void ListParkedVehicles()
+        {
+            List<Vehicle> parkedVehicles = new();
+            foreach (var parkingSpot in _garage.ParkingSpots)
+            {
+                if (parkedVehicles.Contains(parkingSpot.OccupyingVechicle))
+                {
+                    continue;
+                }
+                else
+                {
+                    parkedVehicles.Add(parkingSpot.OccupyingVechicle);
+                }
+            }
+            int parkedVechicleNumber = 1;
+            foreach (var vehicle in parkedVehicles)
+            {
+                Console.WriteLine($"{parkedVechicleNumber}. {vehicle}");
+                parkedVechicleNumber++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,32 +5,9 @@ internal class Program
     static void Main(string[] args)
     {
 
-        ParkingSpace parking = new(15);
-
-        bool runAgain = true;
-        while (runAgain)
-        {
-            Console.WriteLine("P or C");
-            ConsoleKeyInfo keyPress = Console.ReadKey(true);
-            switch (keyPress.Key)
-            {
-                case ConsoleKey.P:
-                    parking.Park(Utilities.GetRandomVehicle());
-                    break;
-                case ConsoleKey.C:
-                    Utilities.CheckoutRandomVehicle(parking);
-                    break;
-                case ConsoleKey.Q:
-                    runAgain = false;
-                    break;
-            }
-            Console.Clear();
-            for (int i = 0; i < parking.Count; i++)
-            {
-                {
-                    Console.WriteLine($"{parking.ParkingSpots[i].ID}: {parking.ParkingSpots[i].OccupiedBy}");
-                }
-            }
-        }
+        ParkingSpace garage = new(15);
+        ConsoleUI UI = new ConsoleUI(garage);
+        UI.Start();
+        
     }
 }
